88d13a473d4a5309cc5f91fb898e5b598c1152b9
Joe Edwards <jedwards@hanekedesign.com>
2017-01-04 16:48:05 -0500

    initializes SliderView file

---
+++ XibPractice/SliderView.swift
@@ -0,0 +1,14 @@
+//
+//  SliderView.swift
+//  XibPractice
+//
+//  Created by Joe Edwards on 1/4/17.
+//  Copyright © 2017 HanekeDesign. All rights reserved.
+//
+
+import UIKit
+
+class SliderView: UIView {
+
+
+}
