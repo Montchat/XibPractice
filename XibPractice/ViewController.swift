5a468a96a5c1ca139dba1ac018eed14e50d80285
Joe Edwards <jedwards@hanekedesign.com>
2017-01-04 16:12:47 -0500

    Initial Commit

---
+++ XibPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XibPractice
+//
+//  Created by Joe Edwards on 1/4/17.
+//  Copyright © 2017 HanekeDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
