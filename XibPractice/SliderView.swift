7184da76ff0c1ac39d4e92a6d49d68f64ea4851d
Joe Edwards <jedwards@hanekedesign.com>
2017-01-04 17:00:04 -0500

    develops logic to load view

--- XibPractice/SliderView.swift
+++ XibPractice/SliderView.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class SliderView: UIView {
+	
+	@IBOutlet weak var question: UILabel!
+	@IBOutlet weak var level: UILabel!
 
+	@IBOutlet weak var slider: UISlider!
+	
+	@IBOutlet weak var minValue: UILabel!
+	@IBOutlet weak var medianValue: UILabel!
+	@IBOutlet weak var maxValue: UILabel!
+	
+	@IBOutlet var values: [UILabel]! // min == 0, median == 1, max == 2
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		for label in labels { 
+		
+	}
+	
+	func config(with question:String, minValue:String, medianValue:String?, maxValue:String) {
+		
+		self.question.text = question
+		self.minValue.text = minValue
+		self.maxValue.text = maxValue
+		
+		if medianValue != nil { self.medianValue.text = medianValue }
+		else { self.medianValue.alpha = 0 }
+		
+	}
 
 }
