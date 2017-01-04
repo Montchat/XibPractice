871590e3f2db8c9e0ca4a6b913ef7eeb504d0a19
Joe Edwards <jedwards@hanekedesign.com>
2017-01-04 16:47:04 -0500

    adds constraints and bottom views to xib file

---
+++ XibPractice/ComponentColors.swift
@@ -0,0 +1,72 @@
+//
+//  ComponentColors.swift
+//  XibPractice
+//
+//  Created by Joe Edwards on 1/4/17.
+//  Copyright © 2017 HanekeDesign. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	
+	class var tapCoolGrey: UIColor {
+		return UIColor(red: 164.0 / 255.0, green: 170.0 / 255.0, blue: 179.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapLightSkyBlue: UIColor {
+		return UIColor(red: 207.0 / 255.0, green: 242.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapDodgerBlue: UIColor {
+		return UIColor(red: 59.0 / 255.0, green: 183.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapMidnight: UIColor {
+		return UIColor(red: 2.0 / 255.0, green: 11.0 / 255.0, blue: 31.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapIceBlue: UIColor {
+		return UIColor(red: 241.0 / 255.0, green: 251.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapRedOrange: UIColor {
+		return UIColor(red: 255.0 / 255.0, green: 60.0 / 255.0, blue: 0.0, alpha: 1.0)
+	}
+	
+	class var tapWeirdGreen: UIColor {
+		return UIColor(red: 46.0 / 255.0, green: 222.0 / 255.0, blue: 119.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapBrightOrange: UIColor {
+		return UIColor(red: 255.0 / 255.0, green: 100.0 / 255.0, blue: 0.0, alpha: 1.0)
+	}
+	
+	class var tapDarkBlueGrey: UIColor {
+		return UIColor(red: 17.0 / 255.0, green: 28.0 / 255.0, blue: 56.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapRobinsEgg: UIColor {
+		return UIColor(red: 108.0 / 255.0, green: 215.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapWhite: UIColor {
+		return UIColor(white: 255.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapSilver: UIColor {
+		return UIColor(red: 200.0 / 255.0, green: 199.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapDandelion: UIColor {
+		return UIColor(red: 255.0 / 255.0, green: 228.0 / 255.0, blue: 0.0, alpha: 1.0)
+	}
+	
+	class var tapBrightBlue: UIColor {
+		return UIColor(red: 0.0, green: 122.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+	}
+	
+	class var tapGunmetal: UIColor {
+		return UIColor(red: 82.0 / 255.0, green: 92.0 / 255.0, blue: 96.0 / 255.0, alpha: 1.0)
+	}
+}
