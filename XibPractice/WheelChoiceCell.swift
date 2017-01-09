7b9cf4ea6c7eb427d335278a484e873032369637
Joe Edwards <jedwards@hanekedesign.com>
2017-01-09 12:38:03 -0500

    initialized wheel choice cell file

---
+++ XibPractice/WheelChoiceCell.swift
@@ -0,0 +1,24 @@
+//
+//  WheelChoiceCell.swift
+//  XibPractice
+//
+//  Created by Joe Edwards on 1/9/17.
+//  Copyright © 2017 HanekeDesign. All rights reserved.
+//
+
+import UIKit
+
+class WheelChoiceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
