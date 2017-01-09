75ccf534779f31a57dd4c29faf66be36e4d8adba
Joe Edwards <jedwards@hanekedesign.com>
2017-01-09 12:36:17 -0500

    adds wheel choice cell to project

---
+++ XibPractice/WheelChoiceViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  WheelChoiceViewModel.swift
+//  thyroid-3D-ios
+//
+//  Created by Daniel Tartaglia on 1/4/17.
+//  Copyright © 2017 haneke. All rights reserved.
+//
+
+import Foundation
+
+
+class WheelChoiceViewModel
+{
+	var title: String = ""
+	var indexes: [Int] = []
+	var choices: [[String]] = []
+	var decimalAfterPosition: Int? = nil
+	var update: ([Int]) -> Void = { _ in }
+	var choice: String {
+		var result = ""
+		for (i, index) in indexes.enumerated() {
+			let element = choices[i][index]
+			if let decimal = decimalAfterPosition, i == decimal {
+				result.append("\(element).")
+			}
+			else {
+				result.append("\(element) ")
+			}
+		}
+		return String(result.characters.dropLast())
+	}
+
+	init(title: String, choices: [[String]], decimalAfterPosition: Int? = nil) {
+		self.title = title
+		self.choices = choices
+		self.decimalAfterPosition = decimalAfterPosition
+	}
+}
