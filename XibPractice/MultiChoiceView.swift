f62be78e2403c509de2cc2b22a230b17ac291014
Joe Edwards <jedwards@hanekedesign.com>
2017-01-09 16:24:07 -0500

    initialized multi choice view file

---
+++ XibPractice/MultiChoiceView.swift
@@ -0,0 +1,21 @@
+//
+//  MultiChoiceView.swift
+//  XibPractice
+//
+//  Created by Joe Edwards on 1/9/17.
+//  Copyright © 2017 HanekeDesign. All rights reserved.
+//
+
+import UIKit
+
+class MultiChoiceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
